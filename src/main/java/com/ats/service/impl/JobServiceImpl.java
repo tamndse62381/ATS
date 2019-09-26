8d158394ebf32e2cea55e5d03818ae3acf41f3a9
Nguyễn Đức Tâm <tamndse62381@gmail.com>
2019-09-26 13:12:49 +0700

    Fix Bug
    - getJobByCompanyID with Date
    - Search CV change String to ListString of param 'skillstring'.

--- src/main/java/com/ats/service/impl/JobServiceImpl.java
+++ src/main/java/com/ats/service/impl/JobServiceImpl.java
@@ -228,7 +228,7 @@ public class JobServiceImpl implements JobService {
         Page<JobDTO> pageDTO = null;
         try {
             LOGGER.info("Begin getJobByCompanyId in Job Repository ");
-            listofJob = jobRepository.getJobByCompanyId(pageable, companyId, "approved");
+            listofJob = jobRepository.getJobByCompanyId(pageable, companyId, "approved", new Date());
             ModelMapper mapper = new ModelMapper();
             java.lang.reflect.Type targetListType = new TypeToken<List<JobDTO>>() {
             }.getType();
@@ -844,7 +844,7 @@ public class JobServiceImpl implements JobService {
         Users users = usersRepository.findOne(EmployerId);
         if (users.getRoleId() == 2) {
             int companyId = listJob.get(0).getCompanyId();
-            jobPage = jobRepository.getJobByCompanyId(null, companyId, "approved");
+            jobPage = jobRepository.getJobByCompanyId(null, companyId, "approved",new Date());
             listJob = jobPage.getContent();
             for (int i = 0; i < listJob.size(); i++) {
                 MainEmpJobDTO dto = new MainEmpJobDTO();
